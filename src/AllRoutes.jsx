82f23baa84ed35f5b3c38a2dcddfea92872e8830
Suresh_G <sureshviji6108@gmail.com>
2024-09-22 11:36:36 +0530

    deploy config

--- src/AllRoutes.jsx
+++ src/AllRoutes.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import DLogin from "./DLogin";
-import App from "./App";  // Example of importing another component for routing
+import FrontPage from "./GlobalFiles/FrontPage";
+import Patient_Details from "./GlobalFiles/Patient_Details";
+
 
 const AllRoutes = () => {
   return (
     <>
       <Routes>
         {/* Define your app routes here */}
-        <Route path="/new_bcc/" element={<DLogin />} />
-        <Route path="/dashboard" element={<App />} /> {/* Example for another route */}
+        <Route path="/new_bcc" element={<DLogin />} />
+        <Route path="/dashboard" element={<FrontPage />} /> {/* Example for another route */}
         {/* Add more routes as needed */}
         
         {/* Fallback route for undefined paths (optional) */}
