36f6323f519cc1d61ba0b7953e2893419c70e7be
Suresh_G <sureshviji6108@gmail.com>
2024-09-22 10:20:40 +0530

    deploy config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import App from './App.jsx';
+import DLogin from './DLogin.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>  {/* Wrap DLogin with BrowserRouter */}
+      <DLogin />
+    </BrowserRouter>
+  </StrictMode>
+);
