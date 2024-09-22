a29bb8a812bd58d3a3c485094b7030d021918481
Suresh_G <sureshviji6108@gmail.com>
2024-09-22 12:16:23 +0530

    deploy config

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import AllRoutes from './AllRoutes'; // Import AllRoutes (which defines all the routing logic)
+import "./App.css";
 
 
 createRoot(document.getElementById('root')).render(
